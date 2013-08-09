0d99c3903d5a62627b585389f9bac715c75be42a
shangxinlei <837387321@qq.com>
2013-08-09 10:45:38 +0800

    box2d
    box2d 样例代码

---
+++ Box2dTest/Box2dTest/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Box2dTestAppController.h
+//  Box2dTest
+//
+//  Created by JigglyGames on 13-8-9.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Box2dTest/Box2dTest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Box2dTestAppController.h
+//  Box2dTest
+//
+//  Created by JigglyGames on 13-8-9.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
